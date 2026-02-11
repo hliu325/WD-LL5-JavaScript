a36baae795ea45ee54a292f678c94c5fb93515ea
Harrison Liu <hliu325@email.asu.edu>
2026-02-11 21:22:46 +0000

    Added JavaScript Testing

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello world!");  
+console.log(console); 
+alert("Welcome to my site!"); 
+console.error("This is my first JavaScript error!");
+alert("harrison"); 
+alert("Teammate 1"); 
+alert("Teammate 2");
+
+alert("Thanks for visiting!");
